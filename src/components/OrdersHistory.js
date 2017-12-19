c92e9de98f2721459bf2753e7276da1ae3e21f06
hoanghieukhtn <hoanghieuhcmus@gmail.com>
2017-12-19 16:35:23 +0700

    test after merge

--- src/components/OrdersHistory.js
+++ src/components/OrdersHistory.js
@@ -1,4 +1,12 @@
 import React from 'react';
 
+const OrdersHistory = ({}) => (
+    <div>
+        <section id="main-content">
+            <section class="wrapper">
+            </section>
+        </section>
+    </div>
+)
 
-
+export default OrdersHistory
